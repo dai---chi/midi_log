d6216acb19765ab1e922d4acef2e8958ca2ee19c
Daichi Katayama <daipanchi@gmail.com>
2016-12-01 01:30:07 +0900

    日時カラムを追加

--- midi_log.rb
+++ midi_log.rb
@@ -2,6 +2,7 @@
 require 'pry'
 require 'json'
 require 'unimidi'
+require 'time'
 
 # Prompt the user
 input = UniMIDI::Input.gets
@@ -24,7 +25,8 @@ while true
   m = input.gets
   midi_data = m[0][:data]
   if have_midi_signal?(midi_data) && !on_key_up?(midi_data) && !sustain_pedal?(midi_data)
-    puts(m[0].to_json)
-    `echo "#{m[0][:data]}\t#{m[0][:timestamp]}" >> ./log/#{Time.now.strftime('%Y_%m_%d')}.tsv`
+    output = "#{m[0][:data]}\t#{m[0][:timestamp]}\t#{Time.new.to_s}"
+    puts(output)
+    `echo #{output} >> ./log/#{Time.now.strftime('%Y_%m_%d')}.tsv`
   end
 end
